3feb7b5230eabf9be70fef86ec38d0c0d140306b
Ashwini Mone <Ashwini.Mone@extentia05.local>
2017-10-25 15:33:10 +0530

    Auth0 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import {Auth} from './services/auth.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+  providers: [Auth]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  constructor(private auth: Auth){
+
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {tokenNotExpired} from 'angular2-jwt';
+
+declare var Auth0Lock: any;
+
+@Injectable()
+export class Auth{
+    //configue Auth0
+    lock = new Auth0Lock('1xyvYevgJ9EaoutouHHkIV3CQsRqT4zD', 'authenapp.auth0.com', {});
+
+    constructor(){
+        // Add callback for lock `authenticated` event
+       this.lock.on("authenticated", (authResult:any) => {
+
+        //get user profile fields
+           this.lock.getUserInfo(authResult.accessToken, (error:any, profile:any){
+               if(error){
+                   console.log(error);
+               }
+               // Set Profile
+               localStorage.setItem('profile', JSON.stringify(profile));
+               // Set Token
+               localStorage.setItem('id_token', authResult.idToken);
+           })
+       });
+   }
+   
+   public login() {
+       // Call the show method to display the widget.
+       this.lock.show();
+   };
+   
+   public authenticated(){
+       return tokenNotExpired();
+   }
+   
+   public logout() {
+       // Remove info from localStorage
+       localStorage.removeItem('id_token');
+       localStorage.removeItem('profile');
+   };
+}
